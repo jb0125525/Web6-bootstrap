7cdf24d812ff1d98e3e4f1bc8ac9a757951b35b2
Jimena Barranco <jb0125525@gmail.com>
2023-08-04 12:11:05 -0600

    feat: funciones change color,name

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,12 +13,38 @@ function changeColor(elementHTML, color) {
 
 }
 
-function eligeColor(elementHTML, colorPredeterminado) {
-    let nuevoColor = prompt("Elige un color para pintarme:", '');
+function changeColorWithPrompt( elementHtml) {
+    const color = prompt("Dime el color en inglés", "yellow");
+    changeTextOfUserColor( elementHtml, color);
+    changeColor( elementHtml, color );
+}
 
-    if (nuevoColor) {
-        elementHTML.style.color = nuevoColor;
-    } else {
-        elementHTML.style.color = colorPredeterminado;
-    }
+function changeTextOfUserColor( element, color ) {
+    element.innerHTML = "<h3>Ahora soy <em>" + color + "</em> </h3>";
+}
+
+function changeTextOfUserColor( element, color ) {
+    element.innerHTML = "<h3>Ahora soy <em>" + color + "</em> </h3>"; elementHTML.innerHTML = "<h3> Ahora soy <em>" + nuevoColor + "</em></h3>";
+}
+
+function resetColor(){
+    const refRedColor = document.getElementById("red-color");
+    const refGreenColor = document.getElementById("green-color");
+    const refPurpleColor = document.getElementById("purple-color");
+    const refUserColor = document.getElementById("user-color");
+
+    refRedColor.style.color = "black";
+
+
+    changeColor(refRedColor,'black');
+    changeColor(refGreenColor,'black');
+    changeColor(refPurpleColor,'black');
+    changeColor(refUserColor,'black');
+    changeTextOfUserColor( refUserColor, 'black');
+}
+
+function changeName(){
+    const userName = prompt("Escribe tu nombre: ");
+    const refGretting = document.getElementById("gretting");
+    refGretting.innerHTML = `Hola ${userName}`;
 }
