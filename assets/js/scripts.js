3c0ded386af1a3f7bcc7bb254875e9ed2502eae5
Jimena Barranco <jb0125525@gmail.com>
2023-08-03 18:07:09 -0600

    feat: funciones onclick

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+console.log("Página en construcción");
+
+function clickPez(){
+    alert("Estamos inspirados trabajando con rolas chidas");
+};
+
+function changeColor( elementHTML, color){
+    // alert("Que emoción");
+    console.log(elementHTML);
+    console.log(color);
+
+    elementHTML.style.color = color;
+
+}
