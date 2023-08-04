6cf4d101b02286f494cf794e13cc0dcb886a1e4e
Jimena Barranco <jb0125525@gmail.com>
2023-08-03 23:14:12 -0600

    feat:funcion para cambiar de color con prompt

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,24 @@
 console.log("Página en construcción");
 
-function clickPez(){
+function clickPez() {
     alert("Estamos inspirados trabajando con rolas chidas");
 };
 
-function changeColor( elementHTML, color){
+function changeColor(elementHTML, color) {
     // alert("Que emoción");
     console.log(elementHTML);
     console.log(color);
 
     elementHTML.style.color = color;
 
+}
+
+function eligeColor(elementHTML, colorPredeterminado) {
+    let nuevoColor = prompt("Elige un color para pintarme:", '');
+
+    if (nuevoColor) {
+        elementHTML.style.color = nuevoColor;
+    } else {
+        elementHTML.style.color = colorPredeterminado;
+    }
 }
