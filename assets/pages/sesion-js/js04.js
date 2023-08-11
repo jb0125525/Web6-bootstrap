e77b55957a818cada3fda4b0c02ebbc26e915482
Jimena Barranco <jb0125525@gmail.com>
2023-08-11 11:05:27 -0600

    cit: condiciones y expresiones

--- assets/pages/sesion-js/js04.js
+++ assets/pages/sesion-js/js04.js
@@ -45,9 +45,9 @@ let array3 = [1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 5, 4, 3, 4, 5, 6, 67, 5, 5, 3, 3, 4,
 
 function findNeedle(haystack) {
   for (let i = 0; i < haystack.length; i++) {
-      if (haystack[i] === "needle") {
-          return "found the needle at position " + i;
-      }
+    if (haystack[i] === "needle") {
+      return "found the needle at position " + i;
+    }
   }
 };
 
@@ -55,6 +55,150 @@ console.log(findNeedle(array1));
 console.log(findNeedle(array2));
 console.log(findNeedle(array3));
 
+// ----------------------- Uso de for... of ------------------
+/* Ejecuta una sentencia por cada elemento de un objeto iterable(array, colección, string).
+Sintaxis:
+    for (const iterator of object) {
+    
+    }
+*/
+console.table(personasCh30);
+
+const myPet = "Kraken";
+// for (const character of myPet  ){
+//     console.log( character );
+// }
+
+// for (const equipo of personasCh30 ) {
+//     console.log( equipo ); // Arreglo de equipos
+//     for (const persona of equipo) {
+//         console.log( persona ); //  Persona de cada equipo
+//     }
+//}
+// ---------------- forEach -------------------------
+/*
+ Método que se utiliza para iterar colecciones, arreglos.
+ Permite ejecutar una función por cada elemento del arreglo.
+
+*/
+console.table(personasEnCh30);
+
+function iterarEquipos(equipo, indice, arreglo) {
+  console.log(`Indice ${indice}: ${equipo} `)
+  equipo.forEach(iterarPersonas);
+  return equipo; // arreglo de cada equipo integrador
+}
+
+function iterarPersonas(persona, indice) {
+  console.log(`Indice P ${indice}: ${persona} `)
+  return persona;
+}
+
+
+// personasEnCh30.forEach( iterarEquipos );
+
+personasEnCh30.forEach((equipo, indexEquipo) =>
+  equipo.forEach((persona, indexPer) =>
+    console.log(` [${indexEquipo}][${indexPer}] : ${persona}`)));
+
+
+myPet.split("").forEach((element) => console.log(element));
+
+rompeCicloSuperior:
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= 10; j++) {
+    if (i === 2 && j === 5) break rompeCicloSuperior;
+    console.log(`${i} * ${j} = ${i * j}`);
+
+  }
+}
+
+// Solo se imprimi el elemento (persona)                    
+//personasEnCh30.forEach( equipo => equipo.forEach( persona=> console.log(`${persona}`)));
+
+// ------------------- Uso de break en ciclos ----------------------------
+// break no detiene la ejecución de la iteración en curso y termina el ciclo.
+
+for (let index = 0; index < 10; index++) {
+  if (index >= 5) break;
+  console.log(index); // 0...4    
+}
+
+// Realizar tablas de multiplicar de 1 al 5 con for tradicional
+/*
+  1 * 1  = 1
+  1 * 2  = 2
+  ...
+  5 * 9  = 45
+  5 * 10 = 50
+*/
+
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(`${i} * ${j} = ${i * j}`);
+  }
+}
+
+console.log("===== Uso de break =========")
+// Realizar la multiplicación hasta el 4
+// 1*1, 1*2, 1*3, 1*4..... 5*3, 5*4
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= 10; j++) {
+    if (j === 5) break;
+    console.log(`${i} * ${j} = ${i * j}`);
+  }
+}
+
+console.log("===== Uso de break con tag =======")
+// Realizar la multiplicación hasta el 2 * 4
+// 1*1, 1*2, 1*3, 1*4..... 2*3, 2*4
+rompeCicloSuperior:
+for (let i = 1; i <= 5; i++) {
+
+  for (let j = 1; j <= 10; j++) {
+    if (i === 2 && j == 5) break rompeCicloSuperior;
+    console.log(`${i} * ${j} = ${i * j}`);
+  }
+}
+
+// -------------- Uso de continue ---------------------
+/*
+ Termina la ejecución en la iteración actual y continua con la próxima iteración.
+ ( o en el tag que se indique).
+*/
+console.log("===== Uso de continue con tag =======")
+// Realizar la multiplicación hasta el  3
+// 1*1, 1*2, 1*3 ..... 2*3... 5*3
+continuaCicloSuperior:
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= 10; j++) {
+    if (j > 3) continue continuaCicloSuperior;
+    console.log(`${i} * ${j} = ${i * j}`);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+// ============== Ciclo Do-While ======================
+/*
+ Crea un bucle que ejecuta una sentencia mientras la condición especificada
+ se cumpla. La evaluación se realiza después de ejecutar la sentencia.
+ Por lo tanto, la sentencia se va a ejecutar por lo menos una vez.
+
+ Sintaxis:
+
+    do {
+        sentencias;
+    } while( condición );
+
+*/
 
 
 
---
+++ assets/pages/sesion-js/js05.js
@@ -0,0 +1,2 @@
+console.log("Bienvenidos a la sesión JS05 Condiciones y expresiones");
+console.log("Ni perdono ni olvido Reto para Ed");
